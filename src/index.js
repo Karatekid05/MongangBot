76d873a1b1f39b064a0f098a9705db53e4d80c96
karatekid <dgrua50@proton.me>
2025-07-30 21:24:23 +0100

    Fix ActionRowBuilder import error in market selector

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits, Collection, EmbedBuilder } = require('discord.js');
+const { Client, GatewayIntentBits, Collection, EmbedBuilder, ActionRowBuilder } = require('discord.js');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
